430825e48f090873aae15ec8a6f95066dbeb5919
Andrea Bertagnolli <andrea.bertagnolli@gmail.com>
2018-05-07 13:25:02 +0200

    First commit

---
+++ elect.js
@@ -0,0 +1,33 @@
+// ./main.js
+const {app, BrowserWindow} = require('electron')
+
+let win = null;
+
+app.on('ready', function () {
+
+  // Initialize the window to our specified dimensions
+  win = new BrowserWindow({width: 1000, height: 600});
+
+  // Specify entry point to default entry point of vue.js
+  win.loadURL('http://localhost:8080');
+
+  // Remove window once app is closed
+  win.on('closed', function () {
+    win = null;
+  });
+
+});
+
+//create the application window if the window variable is null
+app.on('activate', () => {
+  if (win === null) {
+    createWindow()
+  }
+})
+
+//quit the app once closed
+app.on('window-all-closed', function () {
+  if (process.platform != 'darwin') {
+    app.quit();
+  }
+});
