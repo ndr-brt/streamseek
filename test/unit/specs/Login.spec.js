cfda497467773a8e4a799fd671315b015cb28837
Andrea Bertagnolli <andrea.bertagnolli@gmail.com>
2018-05-18 13:58:26 +0200

    Rename files

--- test/unit/specs/Login.spec.js
+++ test/unit/specs/Login.spec.js
@@ -1,9 +1,9 @@
 import Vue from 'vue'
-import HelloWorld from '@/components/HelloWorld'
+import Login from '@/components/Login'
 
-describe('HelloWorld.vue', () => {
+describe('Login.vue', () => {
   it('should render correct contents', () => {
-    const Constructor = Vue.extend(HelloWorld)
+    const Constructor = Vue.extend(Login)
     const vm = new Constructor().$mount()
     expect(vm.$el.querySelector('.hello h1').textContent)
       .toEqual('Welcome to Your Vue.js App')
